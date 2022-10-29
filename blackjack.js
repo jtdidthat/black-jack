39a6e9ade00a02530bc9266250de2e3b0551007d
jtdidthat <jtdidthatbusiness@gmail.com>
2022-10-28 19:30:26 -0700

    Update blackjack.js

--- blackjack.js
+++ blackjack.js
@@ -28,13 +28,13 @@ function startGame(){
 }
 
 function getRandomCard(){
-    let randomNumer = Math.floor( Math.random()* 6) + 1
-    if (randomNumer > 10){
+    let randomNumber = Math.floor( Math.random()* 6) + 1
+    if (randomNumber > 10){
         return 10
-    }else if (randomNumer === 1){
+    }else if (randomNumber === 1){
         return 11
     }else {
-        return randomNumer
+        return randomNumber
     }
 }
 
